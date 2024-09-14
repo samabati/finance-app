703fb00eb1b0e7e7bb15ee09d33595c8d5dcce8a
samabatid <samabatid@gmail.com>
2024-09-14 11:22:44 -0400

    added pots savings micro component

--- src/app/pages/overview/pots-overview/pots-overview.component.ts
+++ src/app/pages/overview/pots-overview/pots-overview.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OverviewCardHeaderComponent } from '../../../components/overview/overview-card-header/overview-card-header.component';
+import { PotsSavingsMicroComponent } from './pots-savings-micro/pots-savings-micro.component';
 
 @Component({
   selector: 'app-pots-overview',
   standalone: true,
-  imports: [OverviewCardHeaderComponent],
+  imports: [OverviewCardHeaderComponent, PotsSavingsMicroComponent],
   templateUrl: './pots-overview.component.html',
   styleUrl: './pots-overview.component.css',
 })
